b298d897d175f1496daaab2947b3fa179146e26a
limbo <pythonsuper@gmail.com>
2023-01-03 19:18:56 +0800

    up

--- light-log-spring-boot-starter/src/main/java/com/limbo/light/log/config/SpringAsyncTraceLogConfig.java
+++ light-log-spring-boot-starter/src/main/java/com/limbo/light/log/config/SpringAsyncTraceLogConfig.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.util.concurrent.Executor;
 
 /**
- * description
+ * Spring异步线程池配置
  *
  * @author limbo
  * @since 2022/12/1 20:28
--- light-log-spring-boot-starter/src/main/java/com/limbo/light/log/interceptor/AsyncMDCTaskDecorator.java
+++ light-log-spring-boot-starter/src/main/java/com/limbo/light/log/interceptor/AsyncMDCTaskDecorator.java
@@ -2,6 +2,7 @@ package com.limbo.light.log.interceptor;
 
 import org.slf4j.MDC;
 import org.springframework.core.task.TaskDecorator;
+import org.springframework.lang.Nullable;
 
 import java.util.Map;
 
@@ -13,12 +14,26 @@ import java.util.Map;
  */
 public class AsyncMDCTaskDecorator implements TaskDecorator {
 
+    /**
+     * <p>
+     * decorate 方法复写
+     * </p>
+     *
+     * sl4j的MDC机制内部基于ThreadLocal实现，当MDC.put()方法调用是只会对当前线程生效
+     * 所以在主线程设置MDC数据，其子线程（线程池）无法获取到，需要在创建子线程前及将MDC数据传递给子线程
+     *
+     * @param runnable 可运行
+     * @return {@link Runnable}
+     */
     @Override
-    public Runnable decorate(Runnable runnable) {
+    public Runnable decorate(@Nullable Runnable runnable) {
+        assert runnable != null;
+        // 在父线程新建子线程之前调用MDC.getCopyOfContextMap()方法将MDC内容取出来传给子线程
         Map<String, String> contextMap = MDC.getCopyOfContextMap();
         return () -> {
             try {
                 if (contextMap != null) {
+                    // 子线程在执行操作前先调用MDC.setContextMap()方法将父线程的MDC内容设置到子线程
                     MDC.setContextMap(contextMap);
                 }
                 runnable.run();
