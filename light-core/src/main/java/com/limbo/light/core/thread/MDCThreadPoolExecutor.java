e1941dff4aa10cb10df6186905a3752dfd98590c
limbo <pythonsuper@gmail.com>
2023-01-04 14:57:21 +0800

    Add nullable

--- light-core/src/main/java/com/limbo/light/core/thread/MDCThreadPoolExecutor.java
+++ light-core/src/main/java/com/limbo/light/core/thread/MDCThreadPoolExecutor.java
@@ -1,12 +1,14 @@
 package com.limbo.light.core.thread;
 
 import org.slf4j.MDC;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 
 import java.util.Map;
 import java.util.concurrent.*;
 
 /**
- * description
+ * MDCThreadPoolExecutor
  *
  * @author limbo
  * @since 2022/12/2 21:18
@@ -24,11 +26,12 @@ public class MDCThreadPoolExecutor extends ThreadPoolExecutor {
     }
 
     @Override
-    public void execute(final Runnable runnable) {
-        // 在异步线程执行前获取父线程MDC上下午
+    public void execute(@Nullable final Runnable runnable) {
+        Assert.notNull(runnable, "MDCThreadPoolExecutor execute runnable is not null.");
+        // 在异步线程执行前获取父线程MDC上下文
         final Map<String, String> context = MDC.getCopyOfContextMap();
         super.execute(() -> {
-            // 父线程的MDC上下午 传子线程
+            // 父线程的MDC上下文传子线程
             MDC.setContextMap(context);
             try {
                 runnable.run();
