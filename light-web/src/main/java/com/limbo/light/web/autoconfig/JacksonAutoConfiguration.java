5225316367acff104b3d5a85b98d00d3abff1fce
limbo <pythonsuper@gmail.com>
2023-01-28 19:08:03 +0800

    jackson config

--- light-web/src/main/java/com/limbo/light/web/autoconfig/JacksonAutoConfiguration.java
+++ light-web/src/main/java/com/limbo/light/web/autoconfig/JacksonAutoConfiguration.java
@@ -1,12 +1,20 @@
 package com.limbo.light.web.autoconfig;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * jackson自动配置类
@@ -29,9 +37,29 @@ public class JacksonAutoConfiguration {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return jacksonObjectMapperBuilder -> {
+            // 若POJO对象的属性值为null，序列化时不进行显示
+            jacksonObjectMapperBuilder.serializationInclusion(JsonInclude.Include.NON_NULL);
+            // DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES相当于配置，JSON串含有未知字段时，反序列化依旧可以成功
+            jacksonObjectMapperBuilder.failOnUnknownProperties(false);
+            // 针对于Date类型，文本格式化
+            jacksonObjectMapperBuilder.simpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+            // 针对于JDK新时间类。序列化时带有T的问题，自定义格式化字符串
+            JavaTimeModule javaTimeModule = new JavaTimeModule();
+            javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            jacksonObjectMapperBuilder.modules(javaTimeModule);
+
             jacksonObjectMapperBuilder.serializerByType(BigInteger.class, ToStringSerializer.instance);
-            jacksonObjectMapperBuilder.serializerByType(BigDecimal.class, ToStringSerializer.instance);
             jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance);
+
+            // 默认关闭，即使用BigDecimal.toString()序列化。开启后，使用BigDecimal.toPlainString序列化，不输出科学计数法的值。
+            jacksonObjectMapperBuilder.featuresToEnable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN);
+            jacksonObjectMapperBuilder.serializerByType(BigDecimal.class, ToStringSerializer.instance);
+
+            // 默认关闭，将char[]数组序列化为String类型。若开启后序列化为JSON数组。
+            jacksonObjectMapperBuilder.featuresToEnable(SerializationFeature.WRITE_CHAR_ARRAYS_AS_JSON_ARRAYS);
+
         };
     }
 
